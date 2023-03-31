8f1a22d1737fb7b76ac894108c7b3345589051ef
Leon van Kammen <leonvankammen@gmail.com>
2023-03-31 16:35:54 +0200

    update documentation

--- src/xrfragment/Parser.hx
+++ src/xrfragment/Parser.hx
@@ -4,18 +4,18 @@ package xrfragment;
 @:keep                                                                     // <- avoids accidental removal by dead code elimination
 
 class Parser {                                                             //  # XR Fragments (key/value params)
-		public static var error:String = "";                                   //   
+    public static var error:String = "";                                   //   
 
     @:keep
     public static function parse(key:String,value:String,resultMap:haxe.DynamicAccess<Dynamic>):Bool {
-			var Frag:Map<String, EReg> = new Map<String, EReg>();                //  | param   | type          | category                | notes                   |
+      var Frag:Map<String, EReg> = new Map<String, EReg>();                //  | param   | type          | category                | notes                   |
                                                                            //  |---------|---------------|-------------------------|-------------------------|
-			Frag.set("prio", Type.isInt);                                        //  | prio    | int (-10..1)  | Asset loading / linking | #include doc/notes/prio.md |
+      Frag.set("prio", Type.isInt);                                        //  | prio    | int (-10..1)  | Asset loading / linking | #include doc/notes/prio.md |
 
-			Frag.set("pos",  Type.isVector);                                     //  | pos     | 3D vector     | HREF navigation/portals |  |
-											                                                     //  
+      Frag.set("pos",  Type.isVector);                                     //  | pos     | 3D vector     | HREF navigation/portals |  |
+                                                                           //  
                                                                            //  # XR Fragments parser
-      if( Frag.exists(key) ){                                              //  
+      if( Frag.exists(key) ){                                              //  note: community parsers will prolly outperform this initial parser :)
         if( Frag.get(key).match(value) ){                                  //  > icanhazcode? yes, see [Parser.hx](./../src/xrfragment/Parser.hx)
           var v:Value = new Value();                                       //  
           guessType(v, value);                                             //  the gist of it:
@@ -44,7 +44,7 @@ class Parser {                                                             //  #
         if( xyz.length > 0 ) v.x = Std.parseFloat(xyz[0]);                //  1. anything else will be treated as string-value 
         if( xyz.length > 1 ) v.y = Std.parseFloat(xyz[1]);                //  1. incompatible value-types will be dropped / not used
         if( xyz.length > 2 ) v.y = Std.parseFloat(xyz[2]);                //  
-      }																																    //  > the xrfragment specification should stay simple enough
+      }                                                                    //  > the xrfragment specification should stay simple enough
                                                                           //  > for anyone to write a parser using either regexes or grammar/lexers
       if( Type.isColor.match(str) ) v.color = str;                        //  > therefore expressions/comprehensions are not supported (max wildcard/comparison operators for queries e.g.)
       if( Type.isFloat.match(str) ) v.float = Std.parseFloat(str);
@@ -52,22 +52,22 @@ class Parser {                                                             //  #
     }
 
 }
-																																					 //  # Parser Value types
-																																					 //  
-											                                                     //  | type | info | format | example                          |
+                                                                           //  # Parser Value types
+                                                                           //  
+                                                                           //  | type | info | format | example                          |
 class Value {                                                              //  |------|------|--------|----------------------------------|
-	public var x:Float;                                                      //  |vector| x,y,z| comma-separated    | #pos=1,2,3           |
-	public var y:Float;                                                      // 
-	public var z:Float;                                                      //
-	public var color:String;                                                 //  |string| color| FFFFFF (hex)      | #fog=5m,FFAACC        |
-	public var string:String;                                                //  |string|      |                   | #q=-sun               |
-	public var int:Int;                                                      //  |int   |      | [-]x[xxxxx]       | #price:>=100          |
-	public var float:Float;                                                  //  |float |      | [-]x[.xxxx] (ieee)| #prio=-20
-	public var args:Array<Value>;                                            //  |array | mixed| \|-separated      | #pos=0,0,0|90,0,0     |
-	public function new(){}                                                  //  
-												                                                   //  > rule for thumb: type-limitations will piggyback JSON limitations (IEEE floatsize e.g.)
+  public var x:Float;                                                      //  |vector| x,y,z| comma-separated    | #pos=1,2,3           |
+  public var y:Float;                                                      // 
+  public var z:Float;                                                      //
+  public var color:String;                                                 //  |string| color| FFFFFF (hex)      | #fog=5m,FFAACC        |
+  public var string:String;                                                //  |string|      |                   | #q=-sun               |
+  public var int:Int;                                                      //  |int   |      | [-]x[xxxxx]       | #price:>=100          |
+  public var float:Float;                                                  //  |float |      | [-]x[.xxxx] (ieee)| #prio=-20
+  public var args:Array<Value>;                                            //  |array | mixed| \|-separated      | #pos=0,0,0|90,0,0     |
+  public function new(){}                                                  //  
+                                                                           //  > rule for thumb: type-limitations will piggyback JSON limitations (IEEE floatsize e.g.)
 }
-																																					 // Regexes:
+                                                                           // Regexes:
 class Type {                                                               //  
   static public var isColor:EReg  = ~/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/; //  1. hex colors are detected using regex `/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/`
   static public var isInt:EReg    = ~/^[0-9]+$/;                           //  1. integers are detected using regex `/^[0-9]+$/`
