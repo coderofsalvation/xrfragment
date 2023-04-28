c96919747023068351797a55567f44639a407aa9
Leon van Kammen <leonvankammen@gmail.com>
2023-04-28 16:04:32 +0200

    update documentation

--- example/assets/utils.js
+++ example/assets/utils.js
@@ -19,3 +19,28 @@ export function loadFile(contentLoaders, multiple){
     input.click();
   }
 }
+
+export function setupConsole($console){
+  $console.style.position = 'absolute'
+  $console.style.display = 'block'
+  $console.style.zIndex   = 1000;
+  $console.style.background = "transparent !important"
+  $console.style.pointerEvents = 'none'
+  $console.style.top = '70px'
+  $console.style.padding = '5px 20px 25px 25px'
+  $console.style.left = $console.style.right = $console.style.bottom = 0;
+  $console.style.color = '#0008';
+  $console.style.fontSize = '12px';
+  $console.style.fontFamily = 'Courier'
+  $console.style.border = '0'
+  $console.innerHTML = "XRFRAGMENT CONSOLE OUTPUT:\n" 
+
+  console.log = ( (log) => function(){
+    let s = new Date().toISOString().substr(11).substr(0,8) + " " + ([...arguments]).join(" ").replace(/.*[0-9]: /,"")
+    log(s)
+    let lines = String($console.innerHTML + "\n"+s).split("\n")
+    while( lines.length > 200 ) lines.shift()
+    $console.innerHTML = lines.join("\n")
+    $console.scrollTop = $console.scrollHeight;
+  })(console.log.bind(console))
+}
