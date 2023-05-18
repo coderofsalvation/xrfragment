b8a41ebce05ff34532f6267b47901045b865cc5a
Leon van Kammen <leonvankammen@gmail.com>
2023-05-18 12:39:47 +0200

    work in progress [might break]

--- dist/xrfragment.three.js
+++ dist/xrfragment.three.js
@@ -854,9 +854,11 @@ xrf.add = (object) => {
 xrf.navigator = {}
 
 xrf.navigator.to = (url,event) => {
+  debugger
   return new Promise( (resolve,reject) => {
     console.log("xrfragment: navigating to "+url)
     let {urlObj,dir,file,hash,ext} = xrf.parseUrl(url)
+    if( xrf.model.file == file ) return resolve(xrf.model) // we're already loaded 
     if( xrf.model && xrf.model.scene ) xrf.model.scene.visible = false
     const Loader = xrf.loaders[ext]
     if( !Loader ) throw 'xrfragment: no loader passed to xrfragment for extension .'+ext 
@@ -865,11 +867,10 @@ xrf.navigator.to = (url,event) => {
     if( dir ) dir = dir[0] == '.' ? dir : `.${dir}`
     const loader = new Loader().setPath( dir )
     loader.load( file, function(model){
+      model.file = file
       xrf.add( model.scene )
       xrf.model = model 
-   //   if( event && event.type == "popstate" ){
-        xrf.navigator.commit( file, hash )
-   //   }
+      xrf.navigator.commit( file, hash )
       resolve(model)
     })
   })
