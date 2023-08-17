7ac91ab5e100fa53fbc536f43238fc2cee12108d
Leon van Kammen <leonvankammen@gmail.com>
2023-08-17 10:03:55 +0200

    update documentation

--- src/3rd/js/three/xrf/pos.js
+++ src/3rd/js/three/xrf/pos.js
@@ -7,7 +7,6 @@ xrf.frag.pos = function(v, opts){
     if( v.args ) v = v.args[ xrf.roundrobin(v,model) ]
 
     frag.q.getObjects().map( (o) => {
-      console.dir(o)
       // if object has no parent (name == 'Scene') use absolute positioning, otherwise relative to parent
       o.position.x = o.parent.name == 'Scene' ? v.x : o.positionOriginal.x + v.x
       o.position.y = o.parent.name == 'Scene' ? v.z : o.positionOriginal.y + v.z
