813c68a5e57058d95d3e1bd1d63a87f42935fd3a
coderofsalvation <coderofsalvation@users.noreply.github.com>
2023-11-03 15:40:50 +0000

    deploy: 45d94906385a6c6042400d3a7f268959cc0067c7

--- beta/.vimrc
+++
@@ -1,4 +0,0 @@
-noremap <silent> <F8> :!haxe --no-output %<CR>
-noremap <silent> <F9> :!./make build_js<CR>
-noremap <silent> <F10> :!./make && echo OK && ./make tests<CR>
-noremap <silent> <F11> :!./make tests \| less<CR>
--- beta/src/3rd/js/three/xrmacro/bg.js
+++
@@ -1,6 +0,0 @@
-xrf.addEventListener('bg', (opts) => {
-  let { frag, mesh, model, camera, scene, renderer, THREE} = opts
-  console.log("└ bg "+v.x+","+v.y+","+v.z);
-  if( scene.background ) delete scene.background
-  scene.background = new THREE.Color( v.x, v.y, v.z )
-})
